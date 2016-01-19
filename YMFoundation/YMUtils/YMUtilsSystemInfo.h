a4b45b1fa38b7e0a80c7d851a12bc49db53f7c01
Ryan Guo <ryan@islandzero.net>
2016-01-19 13:01:48 +0800

    add screen size

--- YMFoundation/YMUtils/YMUtilsSystemInfo.h
+++ YMFoundation/YMUtils/YMUtilsSystemInfo.h
@@ -10,9 +10,13 @@
 
 #pragma mark - Macros
 
+#pragma mark - App
+
 #define AppBundleIdentifier ([NSBundle mainBundle].infoDictionary[@"CFBundleIdentifier"])
 #define AppVersionShort     ([NSBundle mainBundle].infoDictionary[@"CFBundleShortVersionString"])
 #define AppVersionLong      ([NSBundle mainBundle].infoDictionary[@"CFBundleVersion"])
+
+#pragma mark - System Version
 
 #define SystemVersionEqualTo(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
 #define SystemVersionGreaterThan(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
@@ -20,8 +24,18 @@
 #define SystemVersionLessThan(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
 #define SystemVersionLessThanOrEqualTo(v)        ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
 
+#pragma mark - Paths
+
 #define AppCacheDirectory ((NSString*)[NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject])
 #define AppDocumentDirectory ((NSString*)[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject])
+
+#pragma mark - Screen
+
+#define ScreenWidth   CGRectGetWidth([UIScreen mainScreen].bounds)
+#define ScreenHeight  CGRectGetHeight([UIScreen mainScreen].bounds)
+
+#define ScreenScale     ([UIScreen mainScreen].scale)
+#define ScreenPixelSize (1.f/SCREEN_SCALE)
 
 #pragma mark - Functions
 
